ecc4247c4c477697f4727f29440793fadd56ad0d
league <league@wts10.attlocal.net>
2018-06-17 12:38:39 -0700

    part of catrunner

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,4 +1,12 @@
 
 public class CatRunner {
-
+	private int lives;
+	private String name;
+	CatRunner(String name, int lives) {
+		this.name = name;
+		this.lives = lives; 
+	}
+public static void main(String[] args) {
+	
+}
 }
